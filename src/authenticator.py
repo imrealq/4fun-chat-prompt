9f3f2d88b3047eea0d629bd461ca0262427795d1
Q T <quynhtn29@gmail.com>
2024-09-22 14:45:54 +0000

    fix authenticate user

--- src/authenticator.py
+++ src/authenticator.py
@@ -7,7 +7,7 @@ def sign_in():
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
     if st.button("Sign In"):
-        if db.authenticate(email, password):
+        if db.authenticate_user(email, password):
             st.success("Success!")
             st.session_state.authenticated = True
             st.rerun()
